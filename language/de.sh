45c2c92916e7c0e04fcff5ea957e88b3f15e3fa4
SmartHome-IoT <39486895+shiot@users.noreply.github.com>
2021-08-22 00:49:24 +0200

    Create de.sh

---
+++ language/de.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#################### Menu ##########
+txt_001="Willkommen bei SmartHome-IoT.net"
+txt_002="Wähle eine option"
+txt_010="Konfiguriere deinen Proxmox HomeServer"
+txt_998="Option nicht gefunden!"
+txt_999="Beenden"
