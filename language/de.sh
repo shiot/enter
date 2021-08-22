c347f0cfe4936fd3a0cc888d72a12fb8c0d17b54
SmartHome-IoT <39486895+shiot@users.noreply.github.com>
2021-08-22 14:44:59 +0200

    Update de.sh

--- language/de.sh
+++ language/de.sh
@@ -3,5 +3,5 @@
 #################### Menu ##########
 txt_001="Willkommen bei SmartHome-IoT.net"
 txt_002="Wähle eine option"
-txt_010="Konfiguriere deinen Proxmox HomeServer"
+txt_010="Konfiguriere meinen Proxmox HomeServer"
 txt_999="Beenden"
